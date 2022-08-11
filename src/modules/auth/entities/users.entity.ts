035ecd43d70ce29e27dd5cb513a0e7f2d330bf44
Eduard <goby145csgo@gmail.com>
2022-08-11 14:54:24 +0200

    add dto & entity

---
+++ src/modules/auth/entities/users.entity.ts
@@ -0,0 +1,15 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Users {
+  @PrimaryGeneratedColumn({
+    name: 'id',
+  })
+  id: number;
+
+  @Column({ nullable: true })
+  email: string;
+
+  @Column({ nullable: true })
+  password: string;
+}
