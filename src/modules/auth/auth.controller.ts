9c350e3935e3ff4f91c30facba410e620f1a9b6a
Eduard <goby145csgo@gmail.com>
2022-08-12 23:01:50 +0200

    fix var name

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -4,11 +4,11 @@ import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly AuthService: AuthService) {}
+  constructor(private readonly authService: AuthService) {}
 
   @HttpCode(200)
   @Post('sign-up')
   async signUp(@Body() dto: AuthDto) {
-    return this.AuthService.signUp(dto);
+    return this.authService.signUp(dto);
   }
 }
