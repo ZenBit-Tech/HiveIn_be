d9b122c3eb727de782c5f2842e67e81e573d51c5
Eduard <goby145csgo@gmail.com>
2022-08-13 17:28:49 +0200

    configure JWT

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,9 +3,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Users } from './entities/users.entity';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Users])],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forFeature([Users]),
+    JwtModule.register({
+      secret: process.env.SECRET_KEY,
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
