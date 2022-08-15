f53ce2dc6c703dbc845a29b520c5aa643d2f6eed
Eduard <goby145csgo@gmail.com>
2022-08-15 11:47:49 +0200

    add JWT strategy

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,9 +3,18 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Users } from './entities/users.entity';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Users])],
+  imports: [
+    TypeOrmModule.forFeature([Users]),
+    ConfigModule.forRoot(),
+    JwtModule.register({
+      secret: process.env.SECRET_KEY,
+      signOptions: { expiresIn: '7200s' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
