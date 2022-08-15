a4b299048c3eea35eeb5e9f6ab88c0b7e9dd1f74
Eduard <goby145csgo@gmail.com>
2022-08-15 11:43:24 +0200

    add private route

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,11 +1,26 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Post,
+  UseGuards,
+  Request,
+} from '@nestjs/common';
 import { AuthDto } from './dto/auth.dto';
 import { AuthService } from './auth.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @UseGuards(AuthGuard('jwt'))
+  @HttpCode(200)
+  @Get('private')
+  privateRoute(@Request() req) {
+    return `Your email, ${req.user.email}`;
+  }
   @HttpCode(200)
   @Post('sign-up')
   async signUp(@Body() dto: AuthDto) {
