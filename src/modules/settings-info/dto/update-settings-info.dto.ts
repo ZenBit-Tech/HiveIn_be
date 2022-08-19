9451ce563e50e07245a9fcad7f028fef5edcbcf6
Joao Hinning <jphinning@gmail.com>
2022-08-19 02:12:52 -0300

    Role added to settings info module

--- src/modules/settings-info/dto/update-settings-info.dto.ts
+++ src/modules/settings-info/dto/update-settings-info.dto.ts
@@ -1,13 +1,19 @@
 import { PartialType } from '@nestjs/mapped-types';
 import {
+  IsEnum,
   IsOptional,
   IsPhoneNumber,
   IsString,
   MaxLength,
 } from 'class-validator';
+import { UserRole } from 'src/modules/entities/users.entity';
 import { CreateSettingsInfoDto } from './create-settings-info.dto';
 
 export class UpdateSettingsInfoDto extends PartialType(CreateSettingsInfoDto) {
+  @IsOptional()
+  @IsEnum(UserRole)
+  readonly role: UserRole;
+
   @IsString()
   @IsOptional()
   @MaxLength(50)
