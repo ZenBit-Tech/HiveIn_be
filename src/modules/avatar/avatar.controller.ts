d5a7ca6b444b5c082a6a7aab00fd871f519888a8
Eduard <goby145csgo@gmail.com>
2022-08-20 12:43:06 +0200

    update consts

--- src/modules/avatar/avatar.controller.ts
+++ src/modules/avatar/avatar.controller.ts
@@ -10,7 +10,7 @@ import {
 import { FileInterceptor } from '@nestjs/platform-express';
 import { join } from 'path';
 import { of } from 'rxjs';
-
+import { avatarLink, uploadsDir } from 'src/utils/avatar.consts';
 import { multerOptions } from '../../config/multer.config';
 
 @Controller('avatar')
@@ -18,13 +18,11 @@ export class AvatarController {
   @Post('upload')
   @UseInterceptors(FileInterceptor('avatar', multerOptions))
   photoUpload(@UploadedFile() file: Express.Multer.File) {
-    return `localhost:4000/avatar/${file.filename}`;
+    return `${avatarLink}/${file.filename}`;
   }
 
   @Get(':imgurl')
   getPhoto(@Param('imgurl') imgUrl, @Res() res) {
-    return of(
-      res.sendFile(join(process.cwd(), `${process.env.UPLOADS_DIR}/${imgUrl}`)),
-    );
+    return of(res.sendFile(join(process.cwd(), `${uploadsDir}/${imgUrl}`)));
   }
 }
