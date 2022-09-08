7306f78a0f84b7d4f737acc21ec90f99191558a8
Milka Vladislav <76705901+milkavladislav@users.noreply.github.com>
2022-09-08 11:46:12 +0300

    finished save proposal endpoint

--- src/modules/proposal/dto/create-proposal.dto.ts
+++ src/modules/proposal/dto/create-proposal.dto.ts
@@ -1,16 +1,4 @@
-import {
-  DurationType,
-  EnglishLevel,
-} from 'src/modules/job-post/entities/job-post.entity';
-import {
-  ArrayMinSize,
-  IsArray,
-  IsBoolean,
-  IsEnum,
-  IsInt,
-  IsNumber,
-  IsString,
-} from 'class-validator';
+import { IsNumber, IsString } from 'class-validator';
 
 export class CreateProposalDto {
   @IsString()
@@ -19,10 +7,6 @@ export class CreateProposalDto {
   @IsNumber()
   readonly rate: number;
 
-  // add questions and attachments
   @IsNumber()
-  readonly idFreelancer: number;
-
-  @IsNumber()
-  readonly idClient: number;
+  readonly idJobPost: number;
 }
