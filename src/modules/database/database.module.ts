e70786452ab7e49ce3ee3782993776a2d9443358
Joao Hinning <jphinning@gmail.com>
2022-09-28 15:28:45 -0300

    fix: Auto migrations run only in production

--- src/modules/database/database.module.ts
+++ src/modules/database/database.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { DEV } from 'src/utils/env.consts';
+import { DEV, PROD } from 'src/utils/env.consts';
 
 @Module({
   imports: [
@@ -19,7 +19,7 @@ import { DEV } from 'src/utils/env.consts';
         migrations: ['dist/migrations/**/*{.ts,.js}'],
         timezone: 'Z',
         logging: true,
-        migrationsRun: configService.get<string>('NODE_ENV') != DEV,
+        migrationsRun: configService.get<string>('NODE_ENV') === PROD,
         autoLoadEntities: true,
         synchronize: configService.get<string>('NODE_ENV') === DEV,
         cli: { migrationsDir: 'src/migrations' },
