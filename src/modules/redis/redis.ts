612677f5c1b1ce94399a09d2fd18da8980d84ad5
Joao Hinning <jphinning@gmail.com>
2022-10-18 23:46:24 -0300

    chore: Deployment config added

--- src/modules/redis/redis.ts
+++ src/modules/redis/redis.ts
@@ -3,8 +3,10 @@ import { config } from 'dotenv';
 
 config();
 
+const { REDIS_HOST, REDIS_PORT, REDIS_USERNAME, REDIS_PASSWORD } = process.env;
+
 const redisClient = createClient({
-  url: `redis://@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+  url: `redis://${REDIS_USERNAME}:${REDIS_PASSWORD}@${REDIS_HOST}:${REDIS_PORT}`,
 });
 
 export async function connectClient() {
